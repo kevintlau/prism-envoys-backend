a8b12e959dda0aa287c68e6e5f10a629343aee01
Kevin Lau <lau.kevt@gmail.com>
2021-06-06 23:56:01 -0700

    adds read (select) and update (save) functions

--- controllers/characters.js
+++ controllers/characters.js
@@ -1,7 +1,7 @@
 const Character = require("../models/character");
 
 const index = (req, res) => {
-  Character.find({uid: req.query.uid}, (err, characters) => {
+  Character.find({ uid: req.query.uid }, (err, characters) => {
     res.status(200).json(characters);
   });
 };
@@ -12,15 +12,30 @@ const create = (req, res) => {
   });
 };
 
+const select = (req, res) => {
+  Character.find({ _id: req.params.id }, (err, character) => {
+    res.status(200).json(character);
+  });
+};
+
+const save = (req, res) => {
+  Character.findByIdAndUpdate(req.params.id, req.body, (err, character) => {
+    req.query.uid = character.uid;
+    index(req, res);
+  });
+};
+
 const deleteChar = (req, res) => {
   Character.findByIdAndDelete(req.params.id, (err, character) => {
     req.query.uid = character.uid;
     index(req, res);
-  })
-}
+  });
+};
 
 module.exports = {
   index,
+  select,
   create,
+  save,
   delete: deleteChar,
 };
--- routes/characters.js
+++ routes/characters.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const charactersCtrl = require("../controllers/characters");
 
 const isAuthenticated = (req, res, next) => {
-  if (!req.query.uid) res.status(410).json({error: "not logged in"});
+  if (!req.query.uid) res.status(410).json({ error: "not logged in" });
   next();
-}
+};
 
 router.get("/", isAuthenticated, charactersCtrl.index);
+router.get("/:id", isAuthenticated, charactersCtrl.select);
 router.post("/", charactersCtrl.create);
+router.put("/:id", isAuthenticated, charactersCtrl.save);
 router.delete("/:id", isAuthenticated, charactersCtrl.delete);
 
 module.exports = router;
